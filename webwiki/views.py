3ddd336aba5dac2b865dda141fb2b5a7339dae75
ankur.junkmechanic <ankurcool.unforgiven@gmail.com>
2013-06-08 20:54:33 +0800

    Nearing First Pass

--- webwiki/views.py
+++ webwiki/views.py
@@ -3,6 +3,8 @@ from django.template import loader, RequestContext
 from webwiki.spider import weave, root_nodes
 import threading
 import time
+import math
+import copy
 
 
 lock = threading.Lock()
@@ -24,14 +26,26 @@ def send_response(request):
     template_name = "web1.html"
     root_node = get_root_node()
     num_of_nodes = len(root_node.weighed_links)
+    root_links = copy.deepcopy(root_node.weighed_links)
+    largest = 0
+    for key in root_links.keys():
+        if root_links[key] > 30:
+            root_links[key] = 30
+        if root_links[key] > largest:
+            largest = root_links[key]
+    largest = largest + 4
+    for key in root_links.keys():
+        root_links[key] = largest - root_links[key]
+        root_links[key] = math.exp(math.log10(root_links[key]))
     t = loader.get_template(template_name)
-    c = RequestContext(request, {'root': root_node, 'num': num_of_nodes})
+    c = RequestContext(request, {'root': root_links, 'num': num_of_nodes,
+                                 'root_url': root_node.name})
     return HttpResponse(t.render(c), content_type="application/xhtml+xml")
 
 
 def buildweb(request):
     crawl_over()
-    time.sleep(5)
+    time.sleep(8)
     return send_response(request)
 
 
