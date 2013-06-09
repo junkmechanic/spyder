11d1b141d567a630190df4ff0b8704efa6b54f39
ankur.junkmechanic <ankurcool.unforgiven@gmail.com>
2013-06-10 02:06:22 +0800

    Minor tunings. UI tweak.

--- webwiki/models.py
+++ webwiki/models.py
@@ -2,5 +2,18 @@ from django.db import models
 
 
 class WikiBlob(models.Model):
-    wiki_url = models.CharField(primary_key=True, max_length=255)
-    wiki_name = models.CharField(max_length=255)
+    url = models.CharField(primary_key=True, max_length=255)
+    name = models.CharField(max_length=255)
+    links = models.TextField()
+    weighed_links = models.ForeignKey('WeighedLinks')
+    word_dict = models.ForeignKey('WordDict')
+
+
+class WeighedLinks(models.Model):
+    key = models.CharField(max_length=100)
+    value = models.FloatField()
+
+
+class WordDict(models.Model):
+    key = models.CharField(max_length=100)
+    value = models.FloatField()
